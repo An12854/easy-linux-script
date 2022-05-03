883da73bdbaab95070f8df5853441fe9e69d2f4e
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-03 11:42:23 +0100

    Create script-install-apt.sh

---
+++ script-install-apt.sh
@@ -0,0 +1,1 @@
+sudo apt update && sudo apt remove -y firefox && sudo apt upgrade -y && sudo apt -y install google-chrome && sudo apt install -y discord && sudo apt install -y steam && sudo apt install -y flameshot
