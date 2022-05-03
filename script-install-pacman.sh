a585c9c2dd2993e1453142ecb86c7f156e277f43
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-03 22:04:41 +0100

    Update script-install-pacman.sh

--- script-install-pacman.sh
+++ script-install-pacman.sh
@@ -4,7 +4,8 @@
 # installs google chrome
 # installs discord
 # installs steam
+# installs vlc
 # installs flameshot, a screenshot taking tool
 # installs neofetch, a command-line system information tool
 
-sudo pacman -R --noconfirm firefox && sudo pacman -Syu --noconfirm && git clone https://aur.archlinux.org/google-chrome.git && cd google-chrome && sudo pacman -S --noconfirm base-devel && makepkg -si && sudo pacman -S --noconfirm discord steam flameshot neofetch
+sudo pacman -R --noconfirm firefox && sudo pacman -Syu --noconfirm && git clone https://aur.archlinux.org/google-chrome.git && cd google-chrome && sudo pacman -S --noconfirm base-devel && makepkg -si && sudo pacman -S --noconfirm discord steam flameshot neofetch vlc
