d28093e2ef41b3df97d395e988ece81f2be40009
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-03 11:48:19 +0100

    Update script-install-apt.sh

--- script-install-apt.sh
+++ script-install-apt.sh
@@ -1,1 +1,10 @@
+
+# updates repositories
+# removes firefox
+# upgrades existing packages
+# installs google chrome
+# installs discord
+# installs steam
+# installs flameshot, a screenshot taking tool
+
 sudo apt update && sudo apt remove -y firefox && sudo apt upgrade -y && sudo apt -y install google-chrome && sudo apt install -y discord && sudo apt install -y steam && sudo apt install -y flameshot
