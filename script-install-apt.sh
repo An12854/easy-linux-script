1935c51bf5136d1bde61476930423e72d3a430c6
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-03 12:33:11 +0100

    Update script-install-apt.sh

--- script-install-apt.sh
+++ script-install-apt.sh
@@ -6,5 +6,6 @@
 # installs discord
 # installs steam
 # installs flameshot, a screenshot taking tool
+# installs neofetch, a command-line system information tool 
 
-sudo apt update && sudo apt remove -y firefox && sudo apt upgrade -y && sudo apt -y install google-chrome discord steam flameshot
+sudo apt update && sudo apt remove -y firefox && sudo apt upgrade -y && sudo apt -y install google-chrome discord steam flameshot neofetch
