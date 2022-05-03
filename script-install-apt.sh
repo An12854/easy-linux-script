65495560340b5581d37fa2c3071a9a12a9eafa2d
azzy <67557558+azzyr@users.noreply.github.com>
2022-05-03 14:08:52 +0300

    Update script-install-apt.sh

--- script-install-apt.sh
+++ script-install-apt.sh
@@ -7,4 +7,4 @@
 # installs steam
 # installs flameshot, a screenshot taking tool
 
-sudo apt update && sudo apt remove -y firefox && sudo apt upgrade -y && sudo apt -y install google-chrome && sudo apt install -y discord && sudo apt install -y steam && sudo apt install -y flameshot
+sudo apt update && sudo apt remove -y firefox && sudo apt upgrade -y && sudo apt -y install google-chrome discord steam flameshot
