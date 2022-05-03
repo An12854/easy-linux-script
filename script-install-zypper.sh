2a8e341f3796efc598605c58a03eed41dea0fd8e
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-03 22:05:08 +0100

    Update script-install-zypper.sh

--- script-install-zypper.sh
+++ script-install-zypper.sh
@@ -5,7 +5,8 @@
 # upgrades existing packages
 # installs discord
 # installs steam
+# installs vlc
 # installs flameshot, a screenshot taking tool
 # installs neofetch, a command-line system information tool 
 
-sudo zypper addrepo http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome && sudo zypper refresh -y && sudo zypper in -y wget && wget https://dl.google.com/linux/linux_signing_key.pub && sudo rpm --import linux_signing_key.pub && sudo zypper in -y google-chrome-stable && sudo zypper rm  -y firefox && sudo zypper up -y && sudo zypper install -y discord steam flameshot neofetch
+sudo zypper addrepo http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome && sudo zypper refresh -y && sudo zypper in -y wget && wget https://dl.google.com/linux/linux_signing_key.pub && sudo rpm --import linux_signing_key.pub && sudo zypper in -y google-chrome-stable && sudo zypper rm  -y firefox && sudo zypper up -y && sudo zypper install -y discord steam flameshot neofetch vlc
