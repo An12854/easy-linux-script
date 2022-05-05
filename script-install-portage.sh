c5bc54444941efb5039742c45684d91db17461d5
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-05 15:43:24 +0100

    Update script-install-portage.sh

--- script-install-portage.sh
+++ script-install-portage.sh
@@ -1,11 +1,10 @@
 # Made for distros that use portage like Gentoo
 # updates repositories
 # upgrades existing packages
-# installs google chrome
-# installs discord
+# installs google chromium
 # installs vlc
 # installs flameshot, a screenshot taking tool
 # installs neofetch, a command-line system information tool
 # installs htop, an interactive process viewer
 
-sudo emaint sync && sudo emerge --verbose --update --deep --changed-use @world && sudo emerge www-client/google-chrome net-im/discord-bin media-video/vlc media-gfx/flameshot app-misc/neofetch sys-process/htop
+sudo emerge --tree --verbose --update --newuse --deep --backtrack=100 --autounmask-backtrack=y @world && sudo smart-live-rebuild --no-erraneous-merge && sudo emerge --tree --verbose --deep --backtrack=100 --autounmask-backtrack=y @preserved-rebuild && sudo emerge www-client/chromium-bin media-video/vlc media-gfx/flameshot app-misc/neofetch sys-process/htop
