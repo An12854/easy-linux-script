c9281a34383fa8cc03b400bd6f32af82f7af10b4
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-05 15:23:32 +0100

    Untested

--- script-install-zypper.sh
+++ script-install-zypper.sh
@@ -7,6 +7,7 @@
 # installs steam
 # installs vlc
 # installs flameshot, a screenshot taking tool
-# installs neofetch, a command-line system information tool 
+# installs neofetch, a command-line system information tool
+# installs htop, an interactive process viewer
 
 sudo zypper addrepo http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome && sudo zypper refresh -y && sudo zypper in -y wget && wget https://dl.google.com/linux/linux_signing_key.pub && sudo rpm --import linux_signing_key.pub && sudo zypper in -y google-chrome-stable && sudo zypper rm  -y firefox && sudo zypper up -y && sudo zypper install -y discord steam flameshot neofetch vlc
