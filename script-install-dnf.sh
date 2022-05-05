dbcc7f8c9ca1e614fa4c0fafc96e6f11f2bbaab9
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-05 15:20:04 +0100

    Untested

--- script-install-dnf.sh
+++ script-install-dnf.sh
@@ -7,5 +7,6 @@
 # installs vlc
 # installs flameshot, a screenshot taking tool
 # installs neofetch, a command-line system information tool
+# installs htop, an interactive process viewer
 
-sudo dnf remove -y firefox && sudo dnf upgrade -y && sudo dnf install -y fedora-workstation-repositories && sudo dnf config-manager --set-enabled google-chrome && sudo dnf install -y google-chrome-stable && sudo dnf install -y discord steam flameshot neofetch vlc
+sudo dnf remove -y firefox && sudo dnf upgrade -y && sudo dnf install -y fedora-workstation-repositories && sudo dnf config-manager --set-enabled google-chrome && sudo dnf install -y google-chrome-stable && sudo dnf install -y discord steam flameshot neofetch vlc htop
