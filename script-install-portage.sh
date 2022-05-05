3f6456c0f6328512bc31b3258dcc03474679743e
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-05 14:48:38 +0100

    Update script-install-portage.sh

--- script-install-portage.sh
+++ script-install-portage.sh
@@ -7,4 +7,4 @@
 # installs flameshot, a screenshot taking tool
 # installs neofetch, a command-line system information tool
 
-sudo emaint sync && sudo emerge --ask --verbose --update --deep --changed-use @world && sudo emerge --ask www-client/google-chrome net-im/discord-bin media-video/vlc media-gfx/flameshot app-misc/neofetch
+sudo emaint sync && sudo emerge --verbose --update --deep --changed-use @world && sudo emerge www-client/google-chrome net-im/discord-bin media-video/vlc media-gfx/flameshot app-misc/neofetch
