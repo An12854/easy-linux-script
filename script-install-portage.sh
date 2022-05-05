c3038e85b6bd5f75876c045c9a90dad07b3f2df0
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-05 15:46:34 +0100

    VERY untested

--- script-install-portage.sh
+++ script-install-portage.sh
@@ -7,4 +7,4 @@
 # installs neofetch, a command-line system information tool
 # installs htop, an interactive process viewer
 
-sudo emerge --tree --verbose --update --newuse --deep --backtrack=100 --autounmask-backtrack=y @world && sudo smart-live-rebuild --no-erraneous-merge && sudo emerge --tree --verbose --deep --backtrack=100 --autounmask-backtrack=y @preserved-rebuild && sudo emerge www-client/chromium-bin media-video/vlc media-gfx/flameshot app-misc/neofetch sys-process/htop
+sudo emaint --auto sync && sudo emerge --tree --verbose --update --newuse --deep --backtrack=100 --autounmask-backtrack=y @world && sudo smart-live-rebuild --no-erraneous-merge && sudo emerge --tree --verbose --deep --backtrack=100 --autounmask-backtrack=y @preserved-rebuild && sudo emerge www-client/chromium-bin media-video/vlc media-gfx/flameshot app-misc/neofetch sys-process/htop
