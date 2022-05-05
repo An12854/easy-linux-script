47212b7855ac17177bd5b122b2ee2df008fa4322
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-05 15:22:17 +0100

    Untested

--- script-install-portage.sh
+++ script-install-portage.sh
@@ -6,5 +6,6 @@
 # installs vlc
 # installs flameshot, a screenshot taking tool
 # installs neofetch, a command-line system information tool
+# installs htop, an interactive process viewer
 
-sudo emaint sync && sudo emerge --verbose --update --deep --changed-use @world && sudo emerge www-client/google-chrome net-im/discord-bin media-video/vlc media-gfx/flameshot app-misc/neofetch
+sudo emaint sync && sudo emerge --verbose --update --deep --changed-use @world && sudo emerge www-client/google-chrome net-im/discord-bin media-video/vlc media-gfx/flameshot app-misc/neofetch sys-process/htop
