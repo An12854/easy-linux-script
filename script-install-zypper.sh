d270fb7d65d7cd000260c713581b4eae4c064db4
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-06 20:32:39 +0100

    Update script-install-zypper.sh

--- script-install-zypper.sh
+++ script-install-zypper.sh
@@ -10,4 +10,4 @@
 # installs neofetch, a command-line system information tool
 # installs htop, an interactive process viewer
 
-sudo zypper addrepo http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome && sudo zypper refresh -n && sudo zypper -n in wget && wget https://dl.google.com/linux/linux_signing_key.pub && sudo rpm --import linux_signing_key.pub && sudo zypper -n in google-chrome-stable && sudo zypper -n rm firefox && sudo zypper -n up && sudo zypper -n install discord steam flameshot neofetch vlc htop
+sudo zypper addrepo http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome && sudo zypper refresh && sudo zypper in wget && wget https://dl.google.com/linux/linux_signing_key.pub && sudo rpm --import linux_signing_key.pub && sudo zypper in google-chrome-stable && sudo zypper rm firefox && sudo zypper up && sudo zypper install discord steam flameshot neofetch vlc htop
