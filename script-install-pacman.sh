09c3dbf908297a703c2ebbdb77092a26eccc6b74
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-23 16:12:30 +0100

    Updated Lutris description

--- script-install-pacman.sh
+++ script-install-pacman.sh
@@ -9,6 +9,6 @@
 # installs neofetch, a command-line system information tool
 # installs htop, an interactive process viewer
 # installs wine, a compatibility layer capable of running Windows applications on Linux
-# installs lutris, a video game preservation platform aiming to keep your video game collection up and running for the years to come
+# installs lutris, an open source gaming platform for Linux. It makes gaming on Linux easier by managing, installing and providing optimal settings for games
 
 sudo pacman -R --noconfirm firefox && sudo pacman -Syu --noconfirm && git clone https://aur.archlinux.org/google-chrome.git && cd google-chrome && sudo pacman -S --noconfirm base-devel && makepkg -si && sudo pacman -S --noconfirm discord steam flameshot neofetch vlc htop wine lutris 
