24e3b3e8417ece10a082427da0812aed561dabfd
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-23 16:04:29 +0100

    Added Wine and Lutris

--- script-install-dnf.sh
+++ script-install-dnf.sh
@@ -8,5 +8,7 @@
 # installs flameshot, a screenshot taking tool
 # installs neofetch, a command-line system information tool
 # installs htop, an interactive process viewer
+# installs wine, a compatibility layer capable of running Windows applications on Linux
+# installs lutris, a video game preservation platform aiming to keep your video game collection up and running for the years to come
 
-sudo dnf remove -y firefox && sudo dnf upgrade -y && sudo dnf install -y fedora-workstation-repositories && sudo dnf config-manager --set-enabled google-chrome && sudo dnf install -y google-chrome-stable && sudo dnf install -y discord steam flameshot neofetch vlc htop
+sudo dnf remove -y firefox && sudo dnf upgrade -y && sudo dnf install -y fedora-workstation-repositories && sudo dnf config-manager --set-enabled google-chrome && sudo dnf install -y google-chrome-stable && dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/36/winehq.repo && sudo dnf install -y discord steam flameshot neofetch vlc htop wine-stable lutris
