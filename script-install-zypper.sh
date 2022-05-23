e7395f7136e385be4a8f5c02a7467fc9fa97a3a9
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-23 16:16:32 +0100

    Updated Lutris description

--- script-install-zypper.sh
+++ script-install-zypper.sh
@@ -10,6 +10,6 @@
 # installs neofetch, a command-line system information tool
 # installs htop, an interactive process viewer
 # installs wine, a compatibility layer capable of running Windows applications on Linux
-# installs lutris, a video game preservation platform aiming to keep your video game collection up and running for the years to come
+# installs lutris, an open source gaming platform for Linux. It makes gaming on Linux easier by managing, installing and providing optimal settings for games
 
 sudo zypper addrepo http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome && sudo zypper refresh && sudo zypper in wget && wget https://dl.google.com/linux/linux_signing_key.pub && sudo rpm --import linux_signing_key.pub && sudo zypper in google-chrome-stable && sudo zypper rm firefox && sudo zypper up && sudo zypper install discord steam flameshot neofetch vlc htop wine lutris
