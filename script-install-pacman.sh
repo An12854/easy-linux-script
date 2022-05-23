274b477ddfd64b0770ff684689e093a9d234f38a
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-23 15:58:37 +0100

    Added wine and lutris

--- script-install-pacman.sh
+++ script-install-pacman.sh
@@ -11,4 +11,4 @@
 # installs wine, a compatibility layer capable of running Windows applications on Linux
 # installs lutris, a video game preservation platform aiming to keep your video game collection up and running for the years to come
 
-sudo pacman -R --noconfirm firefox && sudo pacman -Syu --noconfirm && git clone https://aur.archlinux.org/google-chrome.git && cd google-chrome && sudo pacman -S --noconfirm base-devel && makepkg -si && sudo pacman -S --noconfirm discord steam flameshot neofetch vlc htop wine lutris
+sudo pacman -R --noconfirm firefox && sudo pacman -Syu --noconfirm && git clone https://aur.archlinux.org/google-chrome.git && cd google-chrome && sudo pacman -S --noconfirm base-devel && makepkg -si && sudo pacman -S --noconfirm discord steam flameshot neofetch vlc htop wine lutris 
