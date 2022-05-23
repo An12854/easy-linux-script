eb660cbaeccddea01be0a13cce227ce1631c2a5e
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-23 16:11:10 +0100

    Added wine and lutris

--- script-install-portage.sh
+++ script-install-portage.sh
@@ -6,5 +6,7 @@
 # installs flameshot, a screenshot taking tool
 # installs neofetch, a command-line system information tool
 # installs htop, an interactive process viewer
+# installs wine, a compatibility layer capable of running Windows applications on Linux
+# installs lutris, an open source gaming platform for Linux. It makes gaming on Linux easier by managing, installing and providing optimal settings for games
 
-sudo emaint --auto sync && sudo emerge --tree --verbose --update --newuse --deep --backtrack=100 --autounmask-backtrack=y @world && sudo smart-live-rebuild --no-erraneous-merge && sudo emerge --tree --verbose --deep --backtrack=100 --autounmask-backtrack=y @preserved-rebuild && sudo emerge www-client/chromium-bin media-video/vlc media-gfx/flameshot app-misc/neofetch sys-process/htop
+sudo emaint --auto sync && sudo emerge --tree --verbose --update --newuse --deep --backtrack=100 --autounmask-backtrack=y @world && sudo smart-live-rebuild --no-erraneous-merge && sudo emerge --tree --verbose --deep --backtrack=100 --autounmask-backtrack=y @preserved-rebuild && sudo emerge www-client/chromium-bin media-video/vlc media-gfx/flameshot app-misc/neofetch sys-process/htop virtual/wine games-util/lutris
