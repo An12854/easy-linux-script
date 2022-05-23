77b70bdc7183d8646cdd3bb6987f79c993476d31
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-23 15:58:07 +0100

    Added wine and lutris

--- script-install-zypper.sh
+++ script-install-zypper.sh
@@ -9,5 +9,7 @@
 # installs flameshot, a screenshot taking tool
 # installs neofetch, a command-line system information tool
 # installs htop, an interactive process viewer
+# installs wine, a compatibility layer capable of running Windows applications on Linux
+# installs lutris, a video game preservation platform aiming to keep your video game collection up and running for the years to come
 
-sudo zypper addrepo http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome && sudo zypper refresh && sudo zypper in wget && wget https://dl.google.com/linux/linux_signing_key.pub && sudo rpm --import linux_signing_key.pub && sudo zypper in google-chrome-stable && sudo zypper rm firefox && sudo zypper up && sudo zypper install discord steam flameshot neofetch vlc htop
+sudo zypper addrepo http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome && sudo zypper refresh && sudo zypper in wget && wget https://dl.google.com/linux/linux_signing_key.pub && sudo rpm --import linux_signing_key.pub && sudo zypper in google-chrome-stable && sudo zypper rm firefox && sudo zypper up && sudo zypper install discord steam flameshot neofetch vlc htop wine lutris
