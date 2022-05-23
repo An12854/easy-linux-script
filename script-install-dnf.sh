08f288f1e4fde5ca3b511924aa1993b6b23d741e
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-23 16:14:33 +0100

    Updated Lutris description

--- script-install-dnf.sh
+++ script-install-dnf.sh
@@ -9,6 +9,6 @@
 # installs neofetch, a command-line system information tool
 # installs htop, an interactive process viewer
 # installs wine, a compatibility layer capable of running Windows applications on Linux
-# installs lutris, a video game preservation platform aiming to keep your video game collection up and running for the years to come
+# installs lutris, an open source gaming platform for Linux. It makes gaming on Linux easier by managing, installing and providing optimal settings for games
 
 sudo dnf remove -y firefox && sudo dnf upgrade -y && sudo dnf install -y fedora-workstation-repositories && sudo dnf config-manager --set-enabled google-chrome && sudo dnf install -y google-chrome-stable && dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/36/winehq.repo && sudo dnf install -y discord steam flameshot neofetch vlc htop wine-stable lutris
