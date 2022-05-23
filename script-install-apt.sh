03830804a6cc7683d106b046ee6a5082fa4d9058
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-23 16:13:53 +0100

    Updated Lutris description

--- script-install-apt.sh
+++ script-install-apt.sh
@@ -10,6 +10,6 @@
 # installs neofetch, a command-line system information tool 
 # installs htop, an interactive process viewer
 # installs wine, a compatibility layer capable of running Windows applications on Linux
-# installs lutris, a video game preservation platform aiming to keep your video game collection up and running for the years to come
+# installs lutris, an open source gaming platform for Linux. It makes gaming on Linux easier by managing, installing and providing optimal settings for games
 
 sudo add-apt-repository ppa:lutris-team/lutris && sudo apt update && sudo apt remove -y firefox && sudo apt upgrade -y && sudo apt -y install google-chrome discord steam flameshot neofetch vlc htop wine lutris
