c7883459287132aecd0908ee3fc0230903ed5baa
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-23 16:00:47 +0100

    Added wine and lutris

--- script-install-apt.sh
+++ script-install-apt.sh
@@ -9,5 +9,7 @@
 # installs flameshot, a screenshot taking tool
 # installs neofetch, a command-line system information tool 
 # installs htop, an interactive process viewer
+# installs wine, a compatibility layer capable of running Windows applications on Linux
+# installs lutris, a video game preservation platform aiming to keep your video game collection up and running for the years to come
 
-sudo apt update && sudo apt remove -y firefox && sudo apt upgrade -y && sudo apt -y install google-chrome discord steam flameshot neofetch vlc htop
+sudo add-apt-repository ppa:lutris-team/lutris && sudo apt update && sudo apt remove -y firefox && sudo apt upgrade -y && sudo apt -y install google-chrome discord steam flameshot neofetch vlc htop wine lutris
