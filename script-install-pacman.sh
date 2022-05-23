22d3d925868de80d92475a71c43824681a000dc0
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-23 15:56:27 +0100

    Update script-install-pacman.sh

--- script-install-pacman.sh
+++ script-install-pacman.sh
@@ -8,5 +8,7 @@
 # installs flameshot, a screenshot taking tool
 # installs neofetch, a command-line system information tool
 # installs htop, an interactive process viewer
+# installs wine, a compatibility layer capable of running Windows applications on Linux
+# installs lutris, a video game preservation platform aiming to keep your video game collection up and running for the years to come
 
-sudo pacman -R --noconfirm firefox && sudo pacman -Syu --noconfirm && git clone https://aur.archlinux.org/google-chrome.git && cd google-chrome && sudo pacman -S --noconfirm base-devel && makepkg -si && sudo pacman -S --noconfirm discord steam flameshot neofetch vlc htop
+sudo pacman -R --noconfirm firefox && sudo pacman -Syu --noconfirm && git clone https://aur.archlinux.org/google-chrome.git && cd google-chrome && sudo pacman -S --noconfirm base-devel && makepkg -si && sudo pacman -S --noconfirm discord steam flameshot neofetch vlc htop wine lutris
