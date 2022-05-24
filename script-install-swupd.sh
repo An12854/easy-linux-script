9d0c9cb0472d4c40e417995d706d67c25d07579b
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-24 14:48:00 +0100

    Update script-install-swupd.sh

--- script-install-swupd.sh
+++ script-install-swupd.sh
@@ -1,6 +1,6 @@
 # Made for distros that use swupd like Clear Linux
 # installs flatpak, a utility for software deployment and package management for Linux
-# installs firefox
+# installs google chrome
 # installs discord
 # installs steam
 # installs vlc
@@ -10,4 +10,4 @@
 # installs wine, a compatibility layer capable of running Windows applications on Linux
 # installs lutris, an open source gaming platform for Linux. It makes gaming on Linux easier by managing, installing and providing optimal settings for games
 
-sudo swupd bundle-add desktop vlc htop sysadmin-basic wine lutris && flatpak install flathub com.discordapp.Discord com.valvesoftware.Steam org.flameshot.Flameshot
+sudo swupd bundle-add desktop vlc htop sysadmin-basic wine lutris && flatpak install flathub com.discordapp.Discord com.valvesoftware.Steam org.flameshot.Flameshot com.google.Chrome
