b1f2619f8b5f845fb7694631b944bfe762e3e3ca
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-24 14:25:43 +0100

    Update script-install-swupd.sh

--- script-install-swupd.sh
+++ script-install-swupd.sh
@@ -1,12 +1,11 @@
 # Made for distros that use swupd like Clear Linux
-# updates repositories
 # installs discord
 # installs steam
 # installs vlc
 # installs flameshot, a screenshot taking tool
-# installs neofetch, a command-line system information tool 
+# installs sysadmin-basic bundle, common utilities, useful for managing a system
 # installs htop, an interactive process viewer
 # installs wine, a compatibility layer capable of running Windows applications on Linux
 # installs lutris, an open source gaming platform for Linux. It makes gaming on Linux easier by managing, installing and providing optimal settings for games
 
-# in progress
+sudo swupd bundle-add desktop htop sysadmin-basic wine lutris && flatpak install flathub com.discordapp.Discord com.valvesoftware.Steam org.flameshot.Flameshot
