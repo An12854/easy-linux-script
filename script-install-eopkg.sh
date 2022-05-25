2ff8f6176dd29c66c664ebe28e7ece8cfcfecbcc
An12854 <50544887+An12854@users.noreply.github.com>
2022-05-25 21:54:26 +0100

    Create script-install-eopkg.sh

---
+++ script-install-eopkg.sh
@@ -0,0 +1,13 @@
+# Made for distros that use eopkg like Solus Linux
+# removes firefox
+# installs google chrome
+# installs discord
+# installs steam
+# installs vlc
+# installs flameshot, a screenshot taking tool
+# installs sysadmin-basic bundle, common utilities, useful for managing a system
+# installs htop, an interactive process viewer
+# installs wine, a compatibility layer capable of running Windows applications on Linux
+# installs lutris, an open source gaming platform for Linux. It makes gaming on Linux easier by managing, installing and providing optimal settings for games
+
+# in progress
