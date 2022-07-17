b1ae1376340c27c7f36cef843bab0fc4884bd765
An12854 <50544887+An12854@users.noreply.github.com>
2022-07-17 19:06:37 +0100

    Replaced Chromium with Icecat

--- script-install-portage.sh
+++ script-install-portage.sh
@@ -2,7 +2,7 @@
 # This script is very untested, i still haven't had time to use gentoo, run it in a vm if you want to test it and create an issue for any problems
 # updates repositories
 # upgrades existing packages
-# installs google chromium
+# installs gnu icecat
 # installs vlc
 # installs flameshot, a screenshot taking tool
 # installs neofetch, a command-line system information tool
@@ -10,4 +10,4 @@
 # installs wine, a compatibility layer capable of running Windows applications on Linux
 # installs lutris, an open source gaming platform for Linux. It makes gaming on Linux easier by managing, installing and providing optimal settings for games
 
-sudo emaint --auto sync && sudo emerge --tree --verbose --update --newuse --deep --backtrack=100 --autounmask-backtrack=y @world && sudo smart-live-rebuild --no-erraneous-merge && sudo emerge --tree --verbose --deep --backtrack=100 --autounmask-backtrack=y @preserved-rebuild && sudo emerge www-client/chromium-bin media-video/vlc media-gfx/flameshot app-misc/neofetch sys-process/htop virtual/wine games-util/lutris
+sudo emaint --auto sync && sudo emerge --tree --verbose --update --newuse --deep --backtrack=100 --autounmask-backtrack=y @world && sudo smart-live-rebuild --no-erraneous-merge && sudo emerge --tree --verbose --deep --backtrack=100 --autounmask-backtrack=y @preserved-rebuild && sudo emerge www-client/icecat media-video/vlc media-gfx/flameshot app-misc/neofetch sys-process/htop virtual/wine games-util/lutris
