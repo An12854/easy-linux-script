e49e55ad3170fce4574bf9d50c5fdeeb379c54e6
An12854 <50544887+An12854@users.noreply.github.com>
2022-11-28 13:50:04 +0000

    Removed Google Chrome from the script

--- script-install-apt.sh
+++ script-install-apt.sh
@@ -1,8 +1,6 @@
 # Made for distros that use apt like Debian/Ubuntu
 # updates repositories
-# removes firefox, not really my cup of tea
 # upgrades existing packages
-# installs google chrome
 # installs discord
 # installs steam
 # installs vlc
@@ -12,4 +10,4 @@
 # installs wine, a compatibility layer capable of running Windows applications on Linux
 # installs lutris, an open source gaming platform for Linux. It makes gaming on Linux easier by managing, installing and providing optimal settings for games
 
-sudo add-apt-repository ppa:lutris-team/lutris && sudo apt update && sudo apt remove -y firefox && sudo apt upgrade -y && sudo apt -y install google-chrome discord steam flameshot neofetch vlc htop wine lutris
+sudo add-apt-repository ppa:lutris-team/lutris && sudo apt update && sudo apt upgrade -y && sudo apt -y install discord steam flameshot neofetch vlc htop wine lutris
