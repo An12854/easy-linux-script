f3e638fcac3b9d7685b7925bcdd8d70a13af8f52
An12854 <50544887+An12854@users.noreply.github.com>
2022-11-28 14:21:27 +0000

    Removed Google Chrome from the script

--- script-install-dnf.sh
+++ script-install-dnf.sh
@@ -1,7 +1,5 @@
 # Made for distros that use dnf like Fedora/Cent0S
-# removes firefox, not really my cup of tea
 # upgrades existing packages
-# installs google chrome
 # installs discord
 # installs steam
 # installs vlc
@@ -11,4 +9,4 @@
 # installs wine, a compatibility layer capable of running Windows applications on Linux
 # installs lutris, an open source gaming platform for Linux. It makes gaming on Linux easier by managing, installing and providing optimal settings for games
 
-sudo dnf remove -y firefox && sudo dnf upgrade -y && sudo dnf install -y fedora-workstation-repositories && sudo dnf config-manager --set-enabled google-chrome && sudo dnf install -y google-chrome-stable && sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/36/winehq.repo && sudo dnf install -y discord steam flameshot neofetch vlc htop wine-stable lutris
+sudo dnf remove -y firefox && sudo dnf upgrade -y && sudo dnf install -y fedora-workstation-repositories && sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/36/winehq.repo && sudo dnf install -y discord steam flameshot neofetch vlc htop wine-stable lutris
