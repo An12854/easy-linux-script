7fa57568860ad741cbad68b4961c260fd7260a7f
An12854 <50544887+An12854@users.noreply.github.com>
2022-11-28 14:27:31 +0000

    Removed Google Chrome from the script

--- script-install-zypper.sh
+++ script-install-zypper.sh
@@ -1,7 +1,5 @@
 # Made for distros that use zypper like openSUSE
 # updates repositories
-# installs google chrome
-# removes firefox, not really my cup of tea
 # upgrades existing packages
 # installs discord
 # installs steam
@@ -12,4 +10,4 @@
 # installs wine, a compatibility layer capable of running Windows applications on Linux
 # installs lutris, an open source gaming platform for Linux. It makes gaming on Linux easier by managing, installing and providing optimal settings for games
 
-sudo zypper addrepo http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome && sudo zypper refresh && sudo zypper in wget && wget https://dl.google.com/linux/linux_signing_key.pub && sudo rpm --import linux_signing_key.pub && sudo zypper in google-chrome-stable && sudo zypper rm firefox && sudo zypper up && sudo zypper install discord steam flameshot neofetch vlc htop wine lutris
+sudo zypper refresh && sudo zypper up && sudo zypper install discord steam flameshot neofetch vlc htop wine lutris
