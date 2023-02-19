79fdedd0da66fe32f60f262a193069d3fc171763
An12854 <50544887+An12854@users.noreply.github.com>
2023-02-19 15:35:46 +0000

    Update script-install-dnf.sh
    Forgot to remove the firefox uninstallation my bad

--- script-install-dnf.sh
+++ script-install-dnf.sh
@@ -9,4 +9,4 @@
 # installs wine, a compatibility layer capable of running Windows applications on Linux
 # installs lutris, an open source gaming platform for Linux. It makes gaming on Linux easier by managing, installing and providing optimal settings for games
 
-sudo dnf remove -y firefox && sudo dnf upgrade -y && sudo dnf install -y fedora-workstation-repositories && sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/36/winehq.repo && sudo dnf install -y discord steam flameshot neofetch vlc htop wine-stable lutris
+sudo dnf upgrade -y && sudo dnf install -y fedora-workstation-repositories && sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/36/winehq.repo && sudo dnf install -y discord steam flameshot neofetch vlc htop wine-stable lutris
