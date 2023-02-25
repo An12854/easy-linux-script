40404af520763d8f31271d683e88a3a9a441eac8
An12854 <50544887+An12854@users.noreply.github.com>
2023-02-25 21:21:43 +0000

    Added qbittorrent and updated winehq repo to fedora 37

--- script-install-dnf.sh
+++ script-install-dnf.sh
@@ -1,12 +1,13 @@
-# Made for distros that use dnf like Fedora/Cent0S
+# Made for distros that use dnf like Fedora/CentOS
 # upgrades existing packages
-# installs discord
-# installs steam
-# installs vlc
+# installs discord, a chat app
+# installs steam, a video game digital distribution service
+# installs vlc, a media player
 # installs flameshot, a screenshot taking tool
 # installs neofetch, a command-line system information tool
 # installs htop, an interactive process viewer
 # installs wine, a compatibility layer capable of running Windows applications on Linux
 # installs lutris, an open source gaming platform for Linux. It makes gaming on Linux easier by managing, installing and providing optimal settings for games
+# installs qbittorrent, a free and open source alternative to µTorrent
 
-sudo dnf upgrade -y && sudo dnf install -y fedora-workstation-repositories && sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/36/winehq.repo && sudo dnf install -y discord steam flameshot neofetch vlc htop wine-stable lutris
+sudo dnf upgrade -y && sudo dnf install -y fedora-workstation-repositories && sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/37/winehq.repo && sudo dnf install -y discord steam flameshot neofetch vlc htop wine-stable lutris qbittorrent
