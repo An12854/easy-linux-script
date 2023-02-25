9e4f2c92fc6fa827331698e039ba895af4d2bad8
An12854 <50544887+An12854@users.noreply.github.com>
2023-02-25 21:25:59 +0000

    Added qbittorrent

--- script-install-pacman.sh
+++ script-install-pacman.sh
@@ -1,13 +1,14 @@
 # Made for distros that use pacman like Arch Linux
 # updates repositories and upgrades existing packages
 # installs firefox (if the distro doesn't come with it already)
-# installs discord
-# installs steam
-# installs vlc
+# installs discord, a chat app
+# installs steam, a video game digital distribution service
+# installs vlc, a media player
 # installs flameshot, a screenshot taking tool
 # installs neofetch, a command-line system information tool
 # installs htop, an interactive process viewer
 # installs wine, a compatibility layer capable of running Windows applications on Linux
 # installs lutris, an open source gaming platform for Linux. It makes gaming on Linux easier by managing, installing and providing optimal settings for games
+# installs qbittorrent, a free and open source alternative to µTorrent
 
-sudo pacman -Syu --noconfirm && sudo pacman -S --noconfirm firefox discord steam flameshot neofetch vlc htop wine lutris 
+sudo pacman -Syu --noconfirm && sudo pacman -S --noconfirm firefox discord steam flameshot neofetch vlc htop wine lutris qbittorrent
