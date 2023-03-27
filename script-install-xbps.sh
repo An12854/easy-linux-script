b490b79aed44513b5486a5aaea813b7fbc16e696
An12854 <50544887+An12854@users.noreply.github.com>
2023-03-27 17:47:37 +0100

    Update script-install-xbps.sh

--- script-install-xbps.sh
+++ script-install-xbps.sh
@@ -10,5 +10,6 @@ Made for distros that use xbps, like Void Linux
 # installs wine, a compatibility layer capable of running Windows applications on Linux
 # installs lutris, an open source gaming platform for Linux. It makes gaming on Linux easier by managing, installing and providing optimal settings for games
 # installs qbittorrent, a free and open source alternative to µTorrent
+# install nano, a cli text editor
 
-sudo xbps-install -Su && sudo xbps-install -u xbps && sudo xbps-install -Sy void-repo-nonfree void-repo-multilib && sudo xbps-install kde5 kde5-baseapps steam vlc flameshot neofetch htop wine lutris qbittorrent
+sudo xbps-install -Su && sudo xbps-install -u xbps && sudo xbps-install -Sy void-repo-nonfree void-repo-multilib && sudo xbps-install -y steam vlc flameshot neofetch htop wine lutris qbittorrent nano
