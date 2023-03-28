1b2adf2fb5c1a9b7b549e1cbc1e15491e60108bd
An12854 <50544887+An12854@users.noreply.github.com>
2023-03-28 19:35:00 +0100

    Update script-install-portage.sh

--- script-install-portage.sh
+++ script-install-portage.sh
@@ -2,12 +2,13 @@
 # This script is very untested, i still haven't had time to use gentoo, run it in a vm if you want to test it and create an issue for any problems
 # updates repositories
 # upgrades existing packages
-# installs gnu icecat
-# installs vlc
+# installs gnu icecat, a web browser
+# installs vlc, a media player
 # installs flameshot, a screenshot taking tool
 # installs neofetch, a command-line system information tool
 # installs htop, an interactive process viewer
 # installs wine, a compatibility layer capable of running Windows applications on Linux
 # installs lutris, an open source gaming platform for Linux. It makes gaming on Linux easier by managing, installing and providing optimal settings for games
+# installs qbittorrent, a free and open source alternative to µTorrent
 
-sudo emaint --auto sync && sudo emerge --tree --verbose --update --newuse --deep --backtrack=100 --autounmask-backtrack=y @world && sudo smart-live-rebuild --no-erraneous-merge && sudo emerge --tree --verbose --deep --backtrack=100 --autounmask-backtrack=y @preserved-rebuild && sudo emerge --ask www-client/icecat media-video/vlc media-gfx/flameshot app-misc/neofetch sys-process/htop virtual/wine games-util/lutris
+sudo emaint --auto sync && sudo emerge --tree --verbose --update --newuse --deep --backtrack=100 --autounmask-backtrack=y @world && sudo emerge --tree --verbose --deep --backtrack=100 --autounmask-backtrack=y @preserved-rebuild && sudo emerge --ask www-client/icecat media-video/vlc media-gfx/flameshot app-misc/neofetch sys-process/htop virtual/wine games-util/lutris net-p2p/qbittorrent
